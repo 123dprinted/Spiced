d5bc4b5670ae245bd5deea1cdef4a249efcbc246
thereal123dprinter <thediamonds0813@gmail.com>
2023-01-15 19:34:54 -0600

    added actual coffee beans, without fruit, and made both the fruit and beans edible. added texture for the new beans.

--- src/main/java/net/printer/coffee/items/Items.java
+++ src/main/java/net/printer/coffee/items/Items.java
@@ -1,6 +1,7 @@
 package net.printer.coffee.items;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -8,7 +9,9 @@ import net.minecraft.util.Identifier;
 import net.printer.coffee.CoffeeMod;
 
 public class Items {
-    public static final Item RAW_COFFEE_BEANS = registerItem("raw_coffee_beans", new Item(new FabricItemSettings()));
+    public static final Item RAW_COFFEE_BEANS = registerItem("raw_coffee_beans", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(6).saturationModifier(1.2f).build())));
+    public static final Item COFFEE_BEANS = registerItem("coffee_beans", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(.2f).build())));
+
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(CoffeeMod.MOD_ID, name), item);
     }
