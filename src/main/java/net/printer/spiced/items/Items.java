9f11405bda5e7b55774c34b0ee8989dc1704bdac
thereal123dprinter <thediamonds0813@gmail.com>
2023-01-16 02:26:42 -0600

    Added roasted coffee beans with respecive textures, and added an incomplete recipe json.

--- src/main/java/net/printer/spiced/items/Items.java
+++ src/main/java/net/printer/spiced/items/Items.java
@@ -11,6 +11,7 @@ import net.printer.spiced.SpicedMod;
 public class Items {
     public static final Item RAW_COFFEE_BEANS = registerItem("raw_coffee_beans", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(6).saturationModifier(1.2f).build())));
     public static final Item COFFEE_BEANS = registerItem("coffee_beans", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(.2f).build())));
+    public static final Item ROASTED_COFFEE_BEANS = registerItem("roasted_coffee_beans", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(.2f).build())));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(SpicedMod.MOD_ID, name), item);
