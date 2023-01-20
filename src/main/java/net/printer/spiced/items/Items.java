5181777914a5a2e052f7128ef03db145eb2192be
thereal123dprinter <thediamonds0813@gmail.com>
2023-01-19 23:26:25 -0600

    added pepper with no textures, dried pepper flakes to be added as a recipe when i get the knife working

--- src/main/java/net/printer/spiced/items/Items.java
+++ src/main/java/net/printer/spiced/items/Items.java
@@ -12,6 +12,7 @@ public class Items {
     public static final Item RAW_COFFEE_BEANS = registerItem("raw_coffee_beans", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(6).saturationModifier(1.2f).build())));
     public static final Item COFFEE_BEANS = registerItem("coffee_beans", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(.2f).build())));
     public static final Item ROASTED_COFFEE_BEANS = registerItem("roasted_coffee_beans", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(.2f).build())));
+    public static final Item CHILI_PEPPER = registerItem("chili_pepper", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(4).saturationModifier(.6f).build())));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(SpicedMod.MOD_ID, name), item);
